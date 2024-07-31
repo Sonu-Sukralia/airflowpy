0957be0777750c1cc5cdd3474900695de370edcd
sonu <sonukr023@gmail.com>
2024-07-31 19:54:52 +0530

    sparkpi

--- dags/sparkpi.py
+++ dags/sparkpi.py
@@ -3,23 +3,19 @@ from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
 from airflow.utils.dates import days_ago
 
+
 default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    'owner': 'datamasterylab.com',
+    'start_date': datetime(2024, 7, 31),
+    'catchup': True
 }
 
 dag = DAG(
     'spark_kubernetes_example',
-    default_args=default_args,
-    description='A DAG to submit Spark jobs on Kubernetes',
-    schedule_interval=timedelta(days=1),
-    start_date=days_ago(1),
-    tags=['example'],
+    default_args = default_args,
+    schedule=timedelta(days=1)
 )
+
 
 spark_operator = SparkKubernetesOperator(
     task_id='spark_pi',
